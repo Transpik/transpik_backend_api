5826709b5549698c8072b1aebf55a77ee034812d
R4JxF60 <rajithakumaraprog@gmail.com>
2022-09-02 12:37:56 +0530

    add: userCreate handler and test case

--- handlers/usersCreate.js
+++ handlers/usersCreate.js
@@ -1,5 +1,18 @@
 async function usersCreate(request, response) {
-    response.send({});
+
+    //just for testing async requests handling
+    const { email, password } = await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try {
+                resolve(request.body);
+            }catch(err) {
+                reject(err);
+            }
+        }, 4000);
+    });
+    response.code(201).send({email: email, message: "user created sucessfull!"});
 }
+
+
 
 module.exports = usersCreate;
